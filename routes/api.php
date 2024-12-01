a62a30f6d27f0074574778027bcee6954a486970
Marcin Bojara <marcin_5@mail.ru>
2024-12-01 19:33:30 +0100

    Add AuthController with register endpoint
    Created a new AuthController to manage authentication with a register method for handling user registration. Configured a new API route under the 'admin' prefix to use this register method, facilitating user registrations through POST requests.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Admin
+Route::prefix('admin')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+});
+
+// Ambassador
+
+// Checkout
